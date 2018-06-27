40b140120e43d2813996af3f7e0bd531370c681c
JellyfishJeff <prisoeli0@gmail.com>
2018-06-27 11:16:04 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+function add (num1, num2) {
+    return(num1+num2);
+} 
+
+function multiply (num1,num2) {
+    var count = 0;
+    for (var i = 0; i<num1; i++ )
+{
+   count = add(count,num2); 
+    
+}
+return(count);
+}
+
+function power (x,n) {
+var exponent = 1;
+for (var i = 1; i<=n; i++ )
+{
+   exponent = multiply(exponent,x); 
+}
+return (exponent);
+}
+
+function factorial(x) {
+var fact = 1;
+for (var i = 1; i<=x; i++){
+fact = multiply(i,fact);
+}
+return(fact);
+}
+
+function fibonacci (x){
+var One = 0;
+var Two = 1;
+var track = 0;
+ for ( var i=2; i<x; i++){
+     track = add(One,Two);
+     One = Two;
+     Two = track;
+ }
+return(track);
+}
+
+
+
+
+
+
+
+
+
+
